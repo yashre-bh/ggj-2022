8fba7c0b08e5257820027c83f6fb154bccfe9ddb
Shreyaa Sharma <shreyasharma.ss305@gmail.com>
2022-01-30 10:20:16 +0530

    Bullet detection

--- source/Actors/Enemy.gd
+++ source/Actors/Enemy.gd
@@ -16,3 +16,9 @@ func _on_StompDetector_body_entered(body: PhysicsBody2D):
 		return
 	get_node("CollisionShape2D").disabled = true
 	queue_free()
+
+func _on_BulletDetector_body_entered(body: PhysicsBody2D):
+	if body.global_position.y < get_node("BulletDetector").global_position.y:
+		return
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
