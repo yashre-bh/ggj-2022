f9d96ec552f1eebf14bef5781f196f36808a3561
Shreya Bhagat <shreyabhagat2002@gmail.com>
2022-01-30 08:57:46 +0530

    bullet disappears after collision

--- source/Actors/Player.gd
+++ source/Actors/Player.gd
@@ -7,7 +7,7 @@ func _process(delta):
 	look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("fire"):
 		var bullet_instance = bullet.instance()
-		bullet_instance.position = get_global_position()
+		bullet_instance.position = $BulletPointer.get_global_position()
 		bullet_instance.rotation_degrees = rotation_degrees
 		bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed,0).rotated(rotation))
 		get_tree().get_root().add_child(bullet_instance)
