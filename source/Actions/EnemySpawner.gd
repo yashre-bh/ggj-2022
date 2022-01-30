8b3b7a5d36fdaf186dca7ab4c4b353c7c025a3c0
Shreyaa Sharma <shreyasharma.ss305@gmail.com>
2022-01-30 10:02:57 +0530

    Enemy spawning

---
+++ source/Actions/EnemySpawner.gd
@@ -0,0 +1,27 @@
+extends Node
+
+var enemyNo = 1
+var _timer = null
+var rand  = RandomNumberGenerator.new()
+
+func _ready():
+	_timer = Timer.new()
+	add_child(_timer)
+	_timer.connect("timeout", self, "_on_Timer_timeout")
+	rand.randomize()
+	_timer.set_wait_time(rand.randf_range(3.0,5.0)) # timer
+	_timer.set_one_shot(false) # Make sure it loops
+	_timer.start()
+
+func _on_Timer_timeout():
+	var enemyscene = load("res://source/Actors/Enemy.tscn")
+	var screen_size = get_viewport().size
+	
+	for i in range(0, enemyNo):
+		var enemy = enemyscene.instance()
+		var position = get_node("Player").global_transform.origin
+		rand.randomize()
+		var x = rand.randf_range(position.x-200, position.x+500.0) # x-range
+		enemy.position.x = x
+		enemy.position.y =-1.0
+		add_child(enemy)
--- source/Actors/Enemy.gd
+++ source/Actors/Enemy.gd
@@ -2,14 +2,17 @@ extends Actor
 
 func _ready() -> void:
 	set_physics_process(false)
-	velocity.x = -speed.x
+	velocity.x = -speed.x/1.5
 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	if is_on_wall():
 		velocity.x *= -1.0
+	var player_position = get_parent().get_child(1).global_position
+	var enemy_position = get_node(".").global_position
+	if (abs(enemy_position.x - player_position.x) > 600.0):
+		velocity.x *= -1.0
 	velocity.y = move_and_slide(velocity, Vector2.UP).y
-
 
 func _on_StompDetector_body_entered(body: PhysicsBody2D):
 	if body.global_position.y < get_node("StompDetector").global_position.y:
